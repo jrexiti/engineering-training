32afcc8b06705c79c4d4d499dd967daa2fb94aac
Jay Rexiti <jrexiti@totalwine.com>
2023-01-23 13:00:23 -0500

    TT-18 add listener and toggle

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ console.log("Engineering Training!")
 
 
 
-       var buttonID = document.getElementById('modalButton')
+var buttonID = document.getElementById('modalButton')
+var hiddenModal = document.getElementById('modalContainer')
+console.log("modalButton " , buttonID.outerHTML)
 
-       console.log("modalButton " , buttonID.outerHTML)
+buttonID.addEventListener('click', function(){
+       console.log("button clicked!")
+       hiddenModal.classList.toggle("hidden")
+})
+
+
+
+
 
 
 
