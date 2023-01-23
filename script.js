a75c2364a77321ed01a58ec1ba86b23372d3bfde
Jay Rexiti <jayrexiti@Jays-MBP.attlocal.net>
2023-01-23 16:46:50 -0500

    add close button with class tag

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ console.log("Engineering Training!")
 var buttonID = document.getElementById('modalButton')
 var hiddenModal = document.getElementById('modalContainer')
 console.log("modalButton " , buttonID.outerHTML)
-
 buttonID.addEventListener('click', function(){
        console.log("button clicked!")
        hiddenModal.classList.toggle("hidden")
+       
 })
+var closeModalButton = document.getElementsByClassName('close-modal-button')
+console.log("close-modal-button", closeModalButton)
 
 
 
