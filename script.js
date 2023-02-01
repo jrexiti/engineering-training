ea87b3542c347b6dacf186f914a811494ef646df
Jay Rexiti <jrexiti@totalwine.com>
2023-02-01 11:25:59 -0500

    added listener on close button

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ console.log("Engineering Training!")
 
 var buttonID = document.getElementById('modalButton')
 var hiddenModal = document.getElementById('modalContainer')
+var closeModalButton = document.getElementsByClassName('close-modal-button')
+var modalContainer = document.getElementById('modalContainer')
+
 console.log("modalButton " , buttonID.outerHTML)
 buttonID.addEventListener('click', function(){
-       console.log("button clicked!")
+       console.log("Open modal button clicked!")
        hiddenModal.classList.toggle("hidden")
        
 })
-var closeModalButton = document.getElementsByClassName('close-modal-button')
 console.log("close-modal-button", closeModalButton)
+
+closeModalButton[0].addEventListener('click', function(){
+       console.log("Clicked close modal button!")
+       modalContainer.classList.toggle('hidden')
+
+
+
+})
 
 
 
